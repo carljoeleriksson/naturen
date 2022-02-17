5f15570931e989ebc6f49fd0153ed42cbb9b4103
carljoeleriksson <carljoeleriksson@gmail.com>
2022-02-17 07:53:31 +0100

    added addToCart functionality

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -16,63 +16,49 @@ function ProductCard(product:any) {
 
     function alreadyInCart(currentCart: any[]) {
         //Check if product already in cart
-        /* 
-        let isInCart = false;
-        for(var i = 0; i < currentCart.length; i++) {
-            if (currentCart[i].name == product.name) {
-                isInCart = true;
-                break;
-            }
-        }
-        
-        return isInCart
- */     console.log('currentCart in alreadyInCart:', currentCart);
- 
         let itemInCart = currentCart.filter((cartItem: any) => (cartItem.name)
                 .toLowerCase()
                 .includes(product.name.toLowerCase())
-        );
-        console.log("itemInCart: ", itemInCart);
-        
-        return itemInCart
+        )
+        return (itemInCart[0])
     }
 
     function addToCart() {  
-      const cartFromLS:any = localStorage.getItem('cart')
-      let currentCart: any[] = JSON.parse(cartFromLS)
-
+        //Get current cart from localStorage
+        const cartFromLS:any = localStorage.getItem('cart')
+        let currentCart: any[] = JSON.parse(cartFromLS)
+        
+        
       if(currentCart){
-          const itemInCart: any = alreadyInCart(currentCart)
-          if(itemInCart.qty <= 0){
-              console.log('item not in cart');
+            //Find out if the item already in cart, and return it
+            let itemInCart: any = {}
+            itemInCart = alreadyInCart(currentCart)
+            
+          if(!itemInCart && product.stock > 0){
               let newItem = {...product}
               newItem.qty++
               newItem.stock--
               const newCart:any = [newItem, ...currentCart]
               localStorage.setItem('cart', JSON.stringify(newCart))
-          } else if(itemInCart.stock <= 0) {
-              console.log('Out of stock');
-              setIsOutOfStock(true)
-          } else {
-              console.log('item In Cart');
-              let newItem = {...itemInCart[0]}
+
+          } else if(itemInCart.qty > 0 && itemInCart.stock > 0) {
+              let newItem = {...itemInCart}
               newItem.qty++
               newItem.stock--
-
+                
               //remove the object from cart 
               const filteredCart = currentCart.filter(cartItem => {
-                  console.log(cartItem.id);
                   return cartItem.id != product.id;
               })
 
-              console.log('filteredCart:', filteredCart);
               const newCart:any = [newItem, ...filteredCart]
               localStorage.setItem('cart', JSON.stringify(newCart))
+              
+          } else if(itemInCart.stock <= 0) {
+            console.log('Out of stock');
+            setIsOutOfStock(true)
           }
-
       } else {
-          console.log('First product in cart');
-          
           let newItem = {...product}
           newItem.qty = 1
           newItem.stock--
